adbf5fb59c0ebbd02c9b11a93b272b7047f08739
Jamie Carter <jc2820@hotmail.com>
2020-04-19 18:39:31 +0100

    popup shows in browser iframe

--- src/content.js
+++ src/content.js
@@ -1,14 +1,35 @@
 /*global chrome*/
+/* src/content.js */
 
 import React from "react";
 import ReactDOM from "react-dom";
+import Frame, { FrameContextConsumer } from "react-frame-component";
 import "./content.css";
-
 const Main = () => {
   return (
-    <div className={"my-extension"}>
-      <h1>Hello world - My first Extension</h1>
-    </div>
+    <Frame
+      head={[
+        <link
+          type="text/css"
+          rel="stylesheet"
+          href={chrome.runtime.getURL("/static/css/content.css")}
+        ></link>,
+      ]}
+    >
+      <FrameContextConsumer>
+        {
+          // Callback is invoked with iframe's window and document instances
+          ({ document, window }) => {
+            // Render Children
+            return (
+              <div className={"my-extension"}>
+                <h1>Hello world - My first Extension</h1>
+              </div>
+            );
+          }
+        }
+      </FrameContextConsumer>
+    </Frame>
   );
 };
 
@@ -19,17 +40,15 @@ document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
 
 app.style.display = "none";
-chrome.runtime.onMessage.addListener(
-   function(request, sender, sendResponse) {
-      if( request.message === "clicked_browser_action") {
-        toggle();
-      }
-   }
-);
-function toggle(){
-   if(app.style.display === "none"){
-     app.style.display = "block";
-   }else{
-     app.style.display = "none";
-   }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "clicked_browser_action") {
+    toggle();
+  }
+});
+function toggle() {
+  if (app.style.display === "none") {
+    app.style.display = "block";
+  } else {
+    app.style.display = "none";
+  }
 }
